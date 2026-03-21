1e1c9ad418514e388581ec6a50fafaaa604e4f7c
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2026-03-20 21:18:56 -0500

    fix: routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './contexts/user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { UserController } from './contexts/user/infrastructure/adapters/in/http/controllers/user.controller';
-import { UserService } from './contexts/user/application/service/user.service';
-import { InterestController } from './contexts/user/infrastructure/adapters/in/http/controllers/interest.controller';
-import { InterestService } from './contexts/user/application/service/interest.service';
 
 @Module({
   imports: [
