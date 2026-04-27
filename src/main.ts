9f5e0a8bc30ddedb5ba129d953bf0cef6f8ed92b
DanielPatiñoMejia <daniel.patino-m@mail.escuelaing.edu.co>
2026-04-27 18:44:44 -0500

    add: test ulr

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8080', `${process.env.PREPROD_URL}`, `${process.env.PROD_URL}`],
+    origin: ['http://localhost:5173', 'http://localhost:8080', `${process.env.PREPROD_URL}`, `${process.env.PROD_URL}`, 'https://peerly-test.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
